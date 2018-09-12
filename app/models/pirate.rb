6a50022e1c2176ac49864d45b5d9fb3e68d140e4
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-12 18:36:15 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,6 +2,14 @@ class Pirate
   attr_accessor :name, :weight, :height
   PIRATES = []
 
-  
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    STUDENTS << self
+  end
+
+  def self.all
+    STUDENTS
+  end
 
 end
