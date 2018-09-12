58388607becf2750f644fb59bd7cba31a50e63c8
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-12 18:40:58 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,11 +1,12 @@
 class Ship
-  attr_reader :name, :topic
+  attr_reader :name, :type, :booty
 
   SHIPS = []
 
   def initialize(args)
     @name = args[:name]
-    @topic = args[:topic]
+    @type = args[:type]
+    @booty = args[:booty]
     SHIPS << self
   end
 
